e65a4dbcb082adb271f2f0971d2f94df9d8711b1
CozyBurrito <cozyburrito@gmail.com>
2016-03-03 18:48:25 -0700

    Add globals.h

---
+++ Stage03/globals.h
@@ -0,0 +1,26 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+typedef unsigned char UINT8;
+typedef unsigned int  UINT16;
+typedef unsigned long UINT32;
+
+
+#define SCREEN_HEIGHT 400
+#define SCREEN_WIDTH 640
+
+#define UP 'w'
+#define DOWN 's'
+#define LEFT 'a'
+#define RIGHT 'd'
+
+#define NUM_PLAYERS 1
+#define NUM_ENEMIES 1
+
+#define PLAYER_DX 8
+#define PLAYER_DY 8
+
+#define EMEMY_DX 8
+
+
+#endif
