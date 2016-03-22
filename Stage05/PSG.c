45ad76b320bd06f21250f8273ad4614fa3ac0aeb
tuxedotshirt <don.hagen@hotmail.com>
2016-03-22 16:16:08 -0600

    Change music files

--- Stage05/PSG.c
+++ Stage05/PSG.c
@@ -1,38 +1,16 @@
 #include <osbind.h>
 
-#include "notes.h"
+#include "PSG.h"
 #include "globals.h"
-
-UINT16 noteArray[] = {C_5,B_4,C_5,G_4,F_4,
-	                        C_5,B_4,C_5,G_4,F_4,
-							C_5,B_4,C_5,D_5,F_5,/*E_5,*/
-							E_5,C_5,D_5,B_4,G_4,
-							E_5,C_5,D_5,B_4,G_4,
-							G_5,C_5,F_5,E_5,E_5,D_5,D_5,B_4,B_1
-							};
-
-UINT16 timeArray[] = {54,54,56,56,56,
-							54,54,56,56,56,
-							54,54,54,60,60,
-							54,54,56,56,56,
-							54,54,56,56,56,
-							50,50,50,60,60,50,50
-							};
-
-void start_sound(long old_ssp){
-	old_ssp = Super(0);
-	set_tone(0, 248);
-	enable_channel(0, 1, 0);
-	set_volume(0, 11);
-	Super(old_ssp);
-}
 
 void write_psg(int reg, UINT8 val) {
 	volatile char *PSG_reg_select = 0xFF8800;
 	volatile char *PSG_reg_write  = 0xFF8802;
-	
+
+	long old_ssp = Super(0);
 	*PSG_reg_select = reg;
 	*PSG_reg_write  = val;
+	Super(old_ssp);
 }
 							
 void set_tone(int channel, int tuning) {
